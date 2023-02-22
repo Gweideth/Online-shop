d75165216269c8d759a82d02155521efa23f49e7
filipzolnierowicz_PC <filipzolnierowicz@gmail.com>
2023-02-22 16:17:37 +0100

    added pagination

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
 
 
-def post_list(request):  # possibility to change code to simpler
-    # https://docs.djangoproject.com/pl/4.1/topics/class-based-views/generic-display/
-    posts = Post.published.all()
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 5)
+    page = request.GET.get("page")
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request,
                   "shop/post/list.html",
-                  {"posts": posts})
+                  {"page": page,
+                  "posts": posts})
 
 
 def post_detail(request, year, month, day, post):
