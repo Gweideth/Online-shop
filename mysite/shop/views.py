bc75d00e1518d97f4c24870157c715f8e46d8b3f
filipzolnierowicz_PC <filipzolnierowicz@gmail.com>
2023-02-24 16:55:27 +0100

    Added protype web store and images
    Models.py: Added Product and Category
    Urls.py: Added new path to store
    Views.py: Added product_list and product_detail
    settings.py: Added MEDIA_URL and MEDIA_ROOT
    Admin.py: added CategoryAdmin and ProductAdmin
    
    Finally added 2 htmls: detail(product) and list(product)

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from .models import Post, Comments
+from .models import Post, Comments, Category, Product
 from .forms import CommentForm
 
 
@@ -34,3 +34,30 @@ def post_detail(request, year, month, day, post):
                   {"post": post,
                    "comments": comments,
                    "comment_form": comment_form})
+
+# STORE
+
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+
+    return render(request,
+                  'shop/product/detail.html',
+                  {'product': product})
